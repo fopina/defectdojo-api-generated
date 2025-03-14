775d0b20cf8a9edfc9c8ae7229ef138b53e3ae58
Filipe Pina <63779195+fopinappb@users.noreply.github.com>
2025-03-14 14:38:01 +0000

    x

--- support/api_generation/openapi-generator-cli.sh
+++ support/api_generation/openapi-generator-cli.sh
@@ -2,6 +2,9 @@
 
 IMAGE=${OPENAPITOOLS_IMAGE:-openapitools/openapi-generator-cli:v7.12.0}
 
+echo $IMAGE
+
+
 VOL=$(cd $(dirname $0); cd ..; cd ..; pwd)
 
-docker run --rm -v $VOL:/local $IMAGE "$@"
+docker run --rm -e https_proxy -e http_proxy -e no_proxy -v $VOL:/local $IMAGE "$@"

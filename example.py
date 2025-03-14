ad4eebcfc26d2117743520d12f06baef17b77eb4
Filipe Pina <63779195+fopinappb@users.noreply.github.com>
2025-03-14 16:10:11 +0000

    x

--- example.py
+++ example.py
@@ -4,6 +4,7 @@ import os
 
 from defectdojo_api_generated.client import DefectDojo
 
+
 if __name__ == '__main__':
     dojo = DefectDojo('https://demo.defectdojo.org/', os.environ['DOJO_TOKEN'])
     r = dojo.findings_api.findings_list()

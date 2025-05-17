d8ce3298cd5d81e05abc12272bfb6c15876fc3bf
Filipe Pina <hzlu1ot0@duck.com>
2025-05-17 07:21:53 +0100

    simplified generator setup/config

--- support/api_generation/generate.sh
+++ support/api_generation/generate.sh
@@ -2,7 +2,6 @@
 
 set -eo pipefail
 
-TEMPDIR=apiclient.spec
 OPENAPI_PATH=${OPENAPI_PATH:-"/local/support/openapi.json"}
 
 # make sure we're on root dir
@@ -10,20 +9,13 @@ cd $(dirname $0)
 cd ..
 cd ..
 
-rm -fr $TEMPDIR
-
 # add custom templates to full templates
 cp -r ./support/api_generation/custom_templates/ ./support/api_generation/templates.spec/
 ./support/api_generation/openapi-generator-cli.sh \
                            generate \
                            -i "${OPENAPI_PATH}" \
-                           -g python \
                            -c /local/support/api_generation/config.yaml \
-                           --template-dir /local/support/api_generation/templates.spec \
-                           -o /local/$TEMPDIR/
+                           -o /local/
 
-rm -fr defectdojo_api_generated
 rm -fr tests/generated
-mv $TEMPDIR/defectdojo_api_generated .
-rm -fr defectdojo_api_generated/docs
 mv defectdojo_api_generated/test tests/generated

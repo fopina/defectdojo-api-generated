8b0825e5b1ba665fe5027f04317540a38a3e56a3
Filipe Pina <hzlu1ot0@duck.com>
2026-03-22 11:21:51 +0000

    add cli boilerplate

---
+++ docs/cli.md
@@ -0,0 +1,10 @@
+# CLI Reference
+
+This page provides documentation for our command line tools.
+
+::: mkdocs-click
+    :module: defectdojo_api_generated.cli.commands.cli
+    :command: _docs_cli_
+    :prog_name: dojo
+    :list_subcommands: true
+    :depth: 2
---
+++ docs/hooks.py
@@ -0,0 +1,9 @@
+def on_config(config, **kwargs):
+    from defectdojo_api_generated.cli import __main__
+    __main__.discover_commands()
+
+    # TODO: mkdocs-click does not support class nor class methods - create PR for this
+    from defectdojo_api_generated.cli.commands import cli
+    cli._docs_cli_ = cli.CLI.click
+
+    return config
